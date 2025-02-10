61070684c2018358c1685cfe61881012137a0dca
ripon4521 <riponakondo4521@gmail.com>
2025-02-10 19:43:56 +0600

    connect to database

--- server/routes/api/authRoute.js
+++ server/routes/api/authRoute.js
@@ -1,9 +1,8 @@
-import express from 'express';
-import { login } from '../../controllers/authController.js';  // Add the .js extension
- // Ensure correct path for ES module
+import express from "express";
+import { login } from "../../controllers/authController.js"; // Add the .js extension for ES module
 
-const _ = express.Router();
+const authRouter = express.Router();
 
-_.post('/login', login);
+authRouter.post("/login", login);
 
-export default _;
+export default authRouter;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import api from './api/index.js';  // Ensure correct path for ES module
+import express from "express";
+import api from "./api/index.js"; // Ensure correct path for ES module
 
-const _ = express.Router();
-const baseUrl = process.env.BASEURL ||  '/api/v1/';
+const apiRouter = express.Router();
+const baseUrl = process.env.BASEURL || "/api/v1/";
 
-_.use(baseUrl, api);
+apiRouter.use(baseUrl, api);
 
-export default _;
+export default apiRouter;
